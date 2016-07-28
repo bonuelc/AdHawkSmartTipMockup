e25cf83c669c7a87d3ffe6e42775cd5582b71c21
Christopher Bonuel <bonuelc@gmail.com>
2016-07-28 15:42:28 -0600

    Add a blank TipTableViewCell.swift file for custom cells

---
+++ SmartTips/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  SmartTips
+//
+//  Created by Christopher Bonuel on 7/28/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
---
+++ SmartTips/TipTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TipTableViewCell.swift
+//  SmartTips
+//
+//  Created by Christopher Bonuel on 7/28/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class TipTableViewCell: UITableViewCell {
+
+}

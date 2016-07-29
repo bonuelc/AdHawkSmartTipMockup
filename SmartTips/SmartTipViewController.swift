249ff9642640ad6cd09eb692ba0ef3d5f207ee6f
Christopher Bonuel <bonuelc@gmail.com>
2016-07-29 08:56:16 -0600

    Add labels to SmartTipVC

--- SmartTips/SmartTipViewController.swift
+++ SmartTips/SmartTipViewController.swift
@@ -11,7 +11,11 @@ import SwiftyJSON
 
 class SmartTipViewController: UIViewController {
     
-    var data: JSON = JSON([]) 
+    @IBOutlet weak var providerLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var data: JSON = JSON([])
 
     override func viewDidLoad() {
         super.viewDidLoad()

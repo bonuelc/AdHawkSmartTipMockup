b918b343c5940d46d927cb14998c4beef709de56
Christopher Bonuel <bonuelc@gmail.com>
2016-07-28 19:39:42 -0600

    Add/connect SmartTipViewController.swift to new VC

---
+++ SmartTips/SmartTipViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SmartTipViewController.swift
+//  SmartTips
+//
+//  Created by Christopher Bonuel on 7/28/16.
+//  Copyright © 2016 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class SmartTipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
